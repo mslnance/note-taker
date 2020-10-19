37987b9016ca80fe0a42501bd79a7477fb7f168a
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-19 15:31:44 -0700

    Add Heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const notes = require('./db/db');
+const express = require('express');
+const app = express();
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+  });
+
+  app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+  });
+
+app.listen(3001, () => {
+   console.log(`API server now on port 3001!`);
+});
