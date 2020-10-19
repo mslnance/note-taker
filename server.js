f93e1aa6d99a44ebae7d1525f05715e5344d2b8e
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-19 15:37:31 -0700

    correct port for heroku

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const notes = require('./db/db');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-  });
+});
 
-  app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     let results = notes;
     console.log(req.query)
     res.json(results);
-  });
+});
 
-app.listen(3001, () => {
-   console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
