6a3c5377f5aa5e918c37e33040c1979dae0ff92c
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-19 21:30:56 -0700

    create and validate data

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-const notes = require('./db/db');
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('./db/db');
+const generateUniqueId = require("generate-unique-id")
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
@@ -12,6 +19,27 @@ function filterByQuery(query, notesArray) {
       filteredResults = filteredResults.filter(note => note.text === note.text);
     }
     return filteredResults;
+}
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    // return finished code to post route for response
+    return note;
+}
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+    return true;
 }
 
 app.get('/api/notes', (req, res) => {
@@ -31,6 +59,24 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(results);
 });
+
+app.post('/api/notes', (req, res) => {
+    const id = generateUniqueId({
+        length: 4,
+        useLetters: false,
+        useNumbers: true
+      });
+    
+    req.body.id = id;
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
